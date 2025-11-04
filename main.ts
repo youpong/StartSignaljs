bf88beb7bd2f0230cc47c6c30976ddd6b8867f74
youpong <43817652+youpong@users.noreply.github.com>
2025-11-04 21:27:54 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    bluetooth.startTemperatureService()
 })
+function go_wait () {
+	
+}
+function wait_for (duration: number) {
+	
+}
+function light_up(column: number) {
 
+}
+function start_sequence() {
+
+}
+let LED_BRIGHTNESS = 0
+let LIGHT_INTERVAL = 1000
+while (true) {
+    
+}
