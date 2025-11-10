41b99f9a44a4c9c4f3fc0ec16ca5ec25eca8f98d
NAKAJIMA Yusaku <yupong7@gmail.com>
2025-11-11 06:02:05 +0900

    wip

--- main.ts
+++ main.ts
@@ -1,53 +1,75 @@
-let LED_BRIGHTNESS = 5
-let LIGHT_INTERVAL = 1000
+let LED_BRIGHTNESS = 5;
+let LIGHT_INTERVAL = 1000;
 
-function go_wait () {
-    return randint(2000, 3000)
+let jump_start = false;
+
+function go_wait() {
+    return randint(2000, 3000);
 }
-function wait_for (duration: number) {
-    basic.pause(duration)
-    return true
+
+function wait_for(duration: number) {
+    let wait_time = input.runningTime() + duration;
+    while (wait_time > input.runningTime()) {
+        if (jump_start) {
+            return false;
+        }
+        basic.pause(1);
+    }
+    return true;
 }
+
 function light_up(column: number) {
-    led.plot(column, 3)
-    led.plot(column, 4)
-    music.play(music.tonePlayable(Note.C, music.beat(BeatFraction.Quarter)), music.PlaybackMode.InBackground)
+    led.plot(column, 3);
+    led.plot(column, 4);
+    music.play(music.tonePlayable(Note.C,
+        music.beat(BeatFraction.Quarter)),
+        music.PlaybackMode.InBackground);
 }
 
 function start_sequence() {
-    basic.clearScreen()
+    basic.clearScreen();
     for (let column = 0; column != 5; ++column) {
-        if (column != 0) {
-            wait_for(LIGHT_INTERVAL)
+        if (column != 0 && !wait_for(LIGHT_INTERVAL)) {
+            return false;
         }
-        light_up(column)
+        light_up(column);
     }
 
-    wait_for(go_wait())
-    basic.clearScreen()
+    if (!wait_for(go_wait())) {
+        return false;
+    }
+
+    basic.clearScreen();
+    return true;
 }
 
 while (true) {
+    jump_start = false;
+
     while (true) {
         if (input.logoIsPressed()) {
-            break
-        }                
+            break;
+        }
     }
 
     input.onButtonPressed(Button.A, function () {
-        basic.showIcon(IconNames.No)
-        control.reset()
-    })
-    start_sequence()
+        jump_start = true;
+    });
 
-    let start_time = input.runningTime()
+    start_sequence();
+    if (jump_start) {
+        basic.showIcon(IconNames.No);
+        continue;
+    }
+
+    let start_time = input.runningTime();
     input.onButtonPressed(Button.A, function () {
-        let reaction_time = input.runningTime() - start_time
-        basic.showNumber(reaction_time / 1000.0)
-        control.reset()
-    })
+        let reaction_time = input.runningTime() - start_time;
+        basic.showNumber(reaction_time / 1000.0);
+        control.reset();
+    });
 
     while (true) {
-        basic.pause(1000)
+        basic.pause(1000);
     }
 }
